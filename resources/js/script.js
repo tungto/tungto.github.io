5a057edf44b0c0c0f14a3479ae844c4401bd9ac2
Tung To <sifebuh.totung@gmail.com>
2018-07-09 16:49:11 +0700

    fix nav responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,58 @@
-var nav = document.querySelector('.js--main-nav');
-var icon = document.querySelector(".js--nav-icon ");
-var contact = document.getElementById("contact");
-var aboutMe = document.getElementById("about-me");
-var portfolio = document.getElementById("portfolio");
+$(document).ready(function () {
 
-function toggleNav(){
-    nav.classList.toggle("show-main-nav")
-}
+    /*  --------------------// Sticky navigtion ---------------*/
+    var waypoint = new Waypoint({
+        element: $('.js--section-about')[0],
+        handler: function (direction) {
+            console.log('run')
+            if (direction === "down") {
+                $('.navbar').addClass('sticky');
+            } else if (direction === "up") {
+                $('.navbar').removeClass('sticky');
+            }
+        },
+        offset: 100
+    })
 
-icon.addEventListener("click", toggleNav)
 
-document.querySelector("ul.main-nav").addEventListener("click", toggleNav )
+    /* ------------------- Scroll to ----------------------------*/
+    $('.js--scroll-to-about').click(function () {
+        $('html, body').animate({ scrollTop: $('#about-me').offset().top }, 800);
+    });
+
+    $('.js--scroll-to-projects').click(function () {
+        $('html, body').animate({ scrollTop: $('#portfolio').offset().top }, 800);
+    })
+
+
+    // $('.js--scroll-to-blog').click(function () {
+    //     $('html, body').animate({ scrollTop: $('#').offset().top }, 800);
+    // })
+
+    $('.js--scroll-to-contact').click(function () {
+        $('html, body').animate({ scrollTop: $('#contact').offset().top }, 800);
+    })
+
+
+    $('.js--scroll-to-home').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--home').offset().top }, 800);
+    })
+
+    /* -----------------------// Animation on scroll ----------------*/
+
+    var waypoint = new Waypoint({
+        element: $('.js--wp')[0],
+        handler: function (direction) {
+            $('.js--wp').addClass('animated bounceInUp');
+        },
+        offset: 300
+    });
+    /* ----------------------- Mobile nav ----------------*/
+
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200);
+    })
+});
+
+
